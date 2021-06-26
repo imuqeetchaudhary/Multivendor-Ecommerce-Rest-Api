21f286f34e7b10237500fe116a5216333dfeb4c9
Abdul Muqeet Arshad <abduliscoool@gmail.com>
2021-06-26 15:13:04 +0500

    added error message in response

--- validation/product.js
+++ validation/product.js
@@ -1,16 +1,26 @@
 const yup = require("yup")
 
 exports.addProductSchema = yup.object({
-    companyName: yup.string().required(),
-    productTitle: yup.string().required(),
-    description: yup.string().required(),
-    price: yup.number().required(),
-    address: yup.string().required(),
-    state: yup.string().required(),
-    isAvailable: yup.boolean().required(),
-    availableDate: yup.date().required(),
+    companyName: yup.string(),
+    productTitle: yup.string(),
+    description: yup.string(),
+    price: yup.number(),
+    address: yup.string(),
+    state: yup.string(),
+    isAvailable: yup.boolean(),
+    availableDate: yup.date(),
 })
 
 exports.getProductSchema = yup.object({
     productId: yup.string().required(),
+})
+
+exports.searchProductSchema = yup.object({
+    companyName: yup.string(),
+    address: yup.string(),
+    state: yup.string(),
+    pricePerDay: yup.number(),
+    pricePerDay: yup.number(),
+    availableDate: yup.date(),
+    availableDate: yup.date(),
 })
