3384ce87bb13195b6144d62bc2b705ba01ca871d
Abdul Muqeet Arshad <abduliscoool@gmail.com>
2021-06-28 12:09:29 +0500

    changed port number to 8000

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const { app } = require("./app")
 const https = require("https");
 
-const PORT = process.env.PORT || 3000
+const PORT = process.env.PORT || 8000
 
 app.listen(PORT, () => {
     console.log(`Sever started at http://localhost:${PORT}`);
