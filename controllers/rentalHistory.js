283e262cbd3489a83d87dae2298b6f2dae2f609c
Abdul Muqeet Arshad <abduliscoool@gmail.com>
2021-07-19 13:04:17 +0500

    added stripe payment method

--- controllers/rentalHistory.js
+++ controllers/rentalHistory.js
@@ -6,6 +6,7 @@ const { promise } = require("../middlewares/promises")
 const { sendMail } = require("../middlewares/sendmail")
 const fs = require("fs")
 const path = require('path');
+const stripe = require('stripe')('sk_test_51J1POvClkiKKoyU1EwrqRkPchsMA2eXdwSeI7VCQiqCOzOVwqOWoWGS8qCEj1fVQA7WCx1nnoeJD3KfPJHEE0XOG00uMs4G6yS');
 
 exports.getRentalHistoryForVendor = promise(async (req, res) => {
     const rentalHistory = await RentalHistory.find({ vendorId: req.user._id })
@@ -54,7 +55,7 @@ exports.addRental = promise(async (req, res) => {
 
     // to find product to be rented
     const product = await Product.findOne({ _id: body.productId })
-    if (!product) throw new Exceptions.NotFound()
+    if (!product) throw new Exceptions.NotFound("Product not found")
 
     const renterId = req.user._id
     const vendorId = product.userId
@@ -62,77 +63,84 @@ exports.addRental = promise(async (req, res) => {
 
     // to find admin to update his commision later
     const admin = await User.findOne({ _id: adminId })
-    if (!admin) throw new Exceptions.NotFound()
+    if (!admin) throw new Exceptions.NotFound("Admin not found")
 
     // to find renter balance to check either balance is > than product price or not
     const renter = await User.findOne({ _id: renterId })
-    if (!renter) throw new Exceptions.NotFound()
+    if (!renter) throw new Exceptions.NotFound("Renter not found")
 
     // to find vendor to update his balance
     const vendor = await User.findOne({ _id: vendorId })
-    if (!vendor) throw new Exceptions.NotFound()
+    if (!vendor) throw new Exceptions.NotFound("Vendor not found")
 
     const totalPrice = body.totalDays * product.pricePerDay
-    const address = `The product's shipping address for ${renter.name} is ${body.shippingAddress }, ${body.shippingState}. The number of renter is ${renter.number}`
+    const address = `The product's shipping address for ${renter.name} is ${body.shippingAddress}, ${body.shippingState}. The number of renter is ${renter.number}`
     console.log(address);
 
     const adminNewBalance = (2 * adminCommision(totalPrice))
 
     if (product.isAvailable == true) {
-        if (renter.balance > totalPrice) {
 
-            const newRental = new RentalHistory({
-                ...req.body,
-                renterId: renterId,
-                vendorId: vendorId,
-                totalPrice: totalPrice
-            })
+        const newRental = new RentalHistory({
+            ...req.body,
+            renterId: renterId,
+            vendorId: vendorId,
+            totalPrice: totalPrice
+        })
 
-            const updateRenter = await User.updateOne(
-                { _id: renterId },
-                {
-                    $set: {
-                        balance: (renter.balance - totalPrice) - adminCommision(totalPrice) - salesTax(totalPrice)
-                    }
-                })
-            console.log("Successfully updated rentar balance");
+        // const updateRenter = await User.updateOne(
+        //     { _id: renterId },
+        //     {
+        //         $set: {
+        //             balance: (renter.balance - totalPrice) - adminCommision(totalPrice) - salesTax(totalPrice)
+        //         }
+        //     })
+        // console.log("Successfully updated rentar balance");
 
-            const updateVendor = await User.updateOne(
-                { _id: vendorId },
-                {
-                    $set: {
-                        balance: (vendor.balance + totalPrice) - adminCommision(totalPrice) + salesTax(totalPrice)
-                    }
-                })
-            console.log("Successfully updated vendor balance");
+        const paymentIntent = await stripe.paymentIntents.create({
+            amount: (totalPrice + adminCommision(totalPrice) + salesTax(totalPrice)) * 100,
+            currency: 'usd',
+            metadata: { integration_check: 'accept_a_payment' },
+            receipt_email: renter.email,
+            payment_method_types: ['card'],
 
-            const updateAdmin = await User.updateOne(
-                { _id: adminId },
-                {
-                    $set: {
-                        balance: admin.balance + adminNewBalance
-                    }
-                })
-            console.log("Successfully updated admin balance");
+        });
 
-            const updateProduct = await Product.updateOne(
-                { _id: body.productId },
-                {
-                    $set: {
-                        isAvailable: false,
-                        availableDate: body.returningDate
-                    }
+        res.json({ 'client_secret': paymentIntent['client_secret'] })
+
+        const updateVendor = await User.updateOne(
+            { _id: vendorId },
+            {
+                $set: {
+                    balance: (vendor.balance + totalPrice) - adminCommision(totalPrice) + salesTax(totalPrice)
                 }
-            )
-            console.log("Successfully updated product available dates");
+            })
+        console.log("Successfully updated vendor balance");
 
-            await newRental.save()
-            await sendMail(vendor.email, address)
-            res.status(200).json({ message: "Successfully rented product" })
-        }
-        else {
-            throw new Exceptions.BadRequset("You don't have enough balance")
-        }
+        const updateAdmin = await User.updateOne(
+            { _id: adminId },
+            {
+                $set: {
+                    balance: admin.balance + adminNewBalance
+                }
+            })
+        console.log("Successfully updated admin balance");
+
+        const updateProduct = await Product.updateOne(
+            { _id: body.productId },
+            {
+                $set: {
+                    isAvailable: false,
+                    availableDate: body.returningDate
+                }
+            }
+        )
+        console.log("Successfully updated product available dates");
+
+        await newRental.save()
+        await sendMail(vendor.email, address)
+        console.log("Successfully rented product");
+        // res.status(200).json({ message: "Successfully rented product" })
     }
     else {
         throw new Exceptions.BadRequset("Product is not available for rent")
