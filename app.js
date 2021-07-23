34c42c40a87d789cba24fe0fc596a22a1b18e25a
Jazim-Abbas <jazimabbas87@gmail.com>
2021-07-23 12:07:50 +0500

    added basic html markup for chat and create socket client instance ..

--- app.js
+++ app.js
@@ -1,26 +1,27 @@
-const cors = require("cors")
-const express = require("express")
-const bodyParser = require("body-parser")
-const { connection } = require("./db/connection")
+const cors = require("cors");
+const express = require("express");
+const bodyParser = require("body-parser");
+const { connection } = require("./db/connection");
 
-const user = require("./routes/user")
-const product = require("./routes/product")
-const rentalHistory = require("./routes/rentalHistory")
-const productReview = require("./routes/productReview")
+const user = require("./routes/user");
+const product = require("./routes/product");
+const rentalHistory = require("./routes/rentalHistory");
+const productReview = require("./routes/productReview");
 
-const app = express()
+const app = express();
 
-app.use(bodyParser.urlencoded({ extended: true }))
-app.use(bodyParser.json())
-app.use(cors())
+app.use(express.static(__dirname + "/upload"));
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+app.use(cors());
 
 app.get("/", async (req, res) => {
-    res.json({ message: "Multivendor E-commerce Rest Api" })
-})
+  res.json({ message: "Multivendor E-commerce Rest Api" });
+});
 
-app.use("/user", user)
-app.use("/product", product)
-app.use("/rental-history", rentalHistory)
-app.use("/product-review", productReview)
+app.use("/user", user);
+app.use("/product", product);
+app.use("/rental-history", rentalHistory);
+app.use("/product-review", productReview);
 
-module.exports = { app }
+module.exports = { app };
