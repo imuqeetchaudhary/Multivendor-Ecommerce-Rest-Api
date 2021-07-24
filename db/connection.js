0b337be7e350a3faec8b5c7701f85c65bc7b753f
Jazim-Abbas <jazimabbas87@gmail.com>
2021-07-24 12:30:36 +0500

    added mongo uri based on node env i.e. development or production ..

--- db/connection.js
+++ db/connection.js
@@ -1,6 +1,12 @@
-const mongoose = require("mongoose")
+const mongoose = require("mongoose");
 
-exports.connection = mongoose.connect("mongodb://localhost:27017/ecommerece-app", {
-    useNewUrlParser: true,
-    useUnifiedTopology: true
-})
+const DEV_MONGO_URI = "mongodb://localhost:27017/ecommerece-app";
+const PROD_MONGO_URI = "paste-your-production-ready-mongo-uri";
+
+const MONGO_URI =
+  process.env.NODE_ENV === "production" ? PROD_MONGO_URI : DEV_MONGO_URI;
+
+exports.connection = mongoose.connect(MONGO_URI, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
--- routes/product.js
+++ routes/product.js
@@ -1,36 +1,59 @@
-const express = require("express")
-const router = express.Router()
-const product = require("../controllers/product")
-const { authentication } = require("../middlewares/isAuth")
-const { validation } = require("../middlewares/validation")
-const { addProductSchema, getProductSchema, searchProductSchema } = require("../validation/product")
+const express = require("express");
+const router = express.Router();
+const product = require("../controllers/product");
+const { authentication } = require("../middlewares/isAuth");
+const { validation } = require("../middlewares/validation");
+const {
+  addProductSchema,
+  getProductSchema,
+  searchProductSchema,
+} = require("../validation/product");
 
-const multer = require("multer")
-const path = require("path")
+const multer = require("multer");
+const path = require("path");
 
 const storage = multer.diskStorage({
-    destination: "./upload/images",
-    filename: (req, file, fileName) => {
-        return fileName(null, `${file.fieldname}_${Date.now()}${path.extname(file.originalname)}`)
-    }
-})
+  destination: "./upload/",
+  filename: (req, file, fileName) => {
+    return fileName(
+      null,
+      `${file.fieldname}_${Date.now()}${path.extname(file.originalname)}`
+    );
+  },
+});
 
 const upload = multer({
-    storage: storage,
-    fileFilter: function (req, file, callback) {
-        var ext = path.extname(file.originalname);
-        if (ext !== '.png' && ext !== '.jpg' && ext !== '.gif' && ext !== '.jpeg') {
-            return callback(new Error('Only images are allowed'))
-        }
-        callback(null, true)
+  storage: storage,
+  fileFilter: function (req, file, callback) {
+    var ext = path.extname(file.originalname);
+    if (ext !== ".png" && ext !== ".jpg" && ext !== ".gif" && ext !== ".jpeg") {
+      return callback(new Error("Only images are allowed"));
     }
-})
+    callback(null, true);
+  },
+});
 
-router.get("/user-get-all", authentication, product.getAllProductForUser)
-router.get("/admin-get-all", authentication, product.getAllProductForAdmin)
-router.post("/search", validation(searchProductSchema), product.searchProduct)
-router.post("/update", authentication, validation(getProductSchema), product.updateProduct)
-router.post("/get-single", authentication, validation(getProductSchema), product.getProduct)
-router.post("/add", authentication, validation(addProductSchema), upload.single("image"), product.addProduct)
+router.get("/user-get-all", authentication, product.getAllProductForUser);
+router.get("/admin-get-all", authentication, product.getAllProductForAdmin);
+router.post("/search", validation(searchProductSchema), product.searchProduct);
+router.post(
+  "/update",
+  authentication,
+  validation(getProductSchema),
+  product.updateProduct
+);
+router.post(
+  "/get-single",
+  authentication,
+  validation(getProductSchema),
+  product.getProduct
+);
+router.post(
+  "/add",
+  authentication,
+  validation(addProductSchema),
+  upload.single("image"),
+  product.addProduct
+);
 
-module.exports = router
+module.exports = router;
